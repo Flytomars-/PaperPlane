b650d95fa6200ea1f5d6beb04b1e51acf8df901d
Lee Bousfield <ljbousfield@gmail.com>
2014-09-09 19:59:18 -0600

    Added systemd support

--- install.sh
+++ install.sh
@@ -1,19 +1,39 @@
 #!/bin/sh
 
+systemd=$(pidof systemd)
+
+if [ -z "$systemd" ]; then
+	echo "Systemd not detected"
+	echo "Assuming upstart"
+	startCmd="start paperplane"
+	stopCmd="stop paperplane"
+else
+	echo "Systemd detected"
+	startCmd="systemctl start PaperPlane"
+	stopCmd="systemctl stop PaperPlane"
+fi
+
 if [ -d "/var/local/paperplane" ]; then
 	echo "Updating existing install"
-	stop paperplane
+	eval "$stopCmd"
 	cp -R . /var/local/paperplane
 	chown -R paperplane:paperplane /var/local/paperplane
-	start paperplane
+	eval "$startCmd"
 fi
 
 if [ ! -d "/var/local/paperplane" ]; then
 	# Add user
 	adduser --system --group --gecos 'User for PaperPlane webapp' --disabled-password --home /home/paperplane paperplane
 	
-	# Copy upstart config
-	cp upstart.conf /etc/init/paperplane.conf
+	if [ -z "$systemd" ]; then
+		# Copy upstart config
+		cp upstart.conf /etc/init/paperplane.conf
+	else
+		# Copy systemd config
+		cp systemd.service /usr/lib/systemd/system/PaperPlane.service
+		# Enable systemd service
+		systemctl enable PaperPlane
+	fi
 	
 	# Copy application folder and create log file
 	cp -R . /var/local/paperplane
@@ -24,5 +44,5 @@ if [ ! -d "/var/local/paperplane" ]; then
 	cd /var/local/paperplane; npm link
 	chown -R paperplane:paperplane /var/local/paperplane
 	
-	start paperplane
+	eval "$startCmd"
 fi
